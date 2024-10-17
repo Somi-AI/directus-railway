f6eb71e9e97a8062ea22b6a018f5ee308232929b
Dave Poon <davepoon@erlango.com>
2024-10-17 18:18:37 +1100

    Refactor Dockerfile.dev and entrypoint.sh to comment out snapshot restoration as optional.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 npx directus bootstrap
-npx directus schema apply --yes ./snapshots/snapshot.yaml
+# Comment out the following line if you want to sync the snapshot
+# npx directus schema apply --yes ./snapshots/snapshot.yaml
 node cli.js bootstrap && pm2-runtime start ecosystem.config.cjs
